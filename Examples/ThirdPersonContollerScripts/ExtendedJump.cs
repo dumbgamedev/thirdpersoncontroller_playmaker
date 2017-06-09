5a534160f6fa9666b275321f5f39bbefb36c28ab
Eric Vander Wal <eric_vanderwal@hotmail.com>
2017-06-09 12:51:30 +0800

    Add files via upload

---
+++ Examples/ThirdPersonContollerScripts/ExtendedJump.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Opsive.ThirdPersonController.Abilities;
+
+
+public class ExtendedJump : Jump
+{
+	public float Force { get { return m_Force; } set { m_Force = value; } }
+	public float DoubleJumpForce { get { return m_DoubleJumpForce; } set { m_DoubleJumpForce = value; } }
+		
+}
